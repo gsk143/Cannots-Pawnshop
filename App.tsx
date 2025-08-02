66e9ef4fbe894c8f78296bc5ff9d7c3fea04dc73
gsk143 <60306998+gsk143@users.noreply.github.com>
2025-08-02 13:18:53 +0530

    chore: restructure the app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
+import * as React from "react";
+import RootNavigation from "./src/navigation/RootNavigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RootNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/types/Navigation.ts
@@ -0,0 +1,5 @@
+type RootStackParamList = {
+  Home: undefined;
+  Profile: { userId: string };
+  Feed: { sort: "latest" | "top" } | undefined;
+};
