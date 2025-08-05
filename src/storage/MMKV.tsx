52b26e6951a7703a03c84063db104202205806ae
gsk143 <60306998+gsk143@users.noreply.github.com>
2025-08-05 19:12:19 +0530

    chore: remove encryption

--- src/storage/MMKV.tsx
+++ src/storage/MMKV.tsx
@@ -1,6 +1,3 @@
 import { MMKV } from "react-native-mmkv";
 
-export const storage = new MMKV({
-  id: "cannots-pawnshop-storage",
-  encryptionKey: "pawnshop-secret-key",
-});
+export const storage = new MMKV();
